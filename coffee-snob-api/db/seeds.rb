d69e4a1e145dbb9ce643462c55fb090e0d8509e3
David Kim <davidjkim810@gmail.com>
2018-10-26 15:38:10 -0400

    added image url and description to cafe model, changed inflection for cafe plural because it was turning into caves

--- coffee-snob-api/db/seeds.rb
+++ coffee-snob-api/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# t.string :name
+# t.string :address
+# t.string :description
+# t.string :brew_methods
+# t.string :price_range
+# t.string :bean_source
+# t.string :image_url
+
+Cafe.create(name: "Cafe Trois", address: "211 Livingston St, Northvale, NJ 07647", description: "French-inspired cafe that offer french pastries, omelets, and sandwiches", brew_methods: "Espresso, Filter", bean_source: "Unknown", price_range: "Moderate", image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/cAk_FFM6_nm22pYLtI9c8A/o.jpg")
