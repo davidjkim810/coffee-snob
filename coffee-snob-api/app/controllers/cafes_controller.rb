40f914c3cf9522718db124b03dd86a7201a610d2
David Kim <davidjkim810@gmail.com>
2018-10-26 15:46:43 -0400

    added comment and cafe controller

---
+++ coffee-snob-api/app/controllers/cafes_controller.rb
@@ -0,0 +1,22 @@
+class CafesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cafe_params
+  end
+
+end
