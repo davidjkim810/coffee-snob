1e225bdcd63f8821abe1012c9e4f595608334280
David Kim <davidjkim810@gmail.com>
2018-10-26 15:26:22 -0400

    added description and image url to cafe model

--- coffee-snob-api/db/migrate/20181026191629_create_cafes.rb
+++ coffee-snob-api/db/migrate/20181026191629_create_cafes.rb
@@ -3,10 +3,11 @@ class CreateCafes < ActiveRecord::Migration[5.2]
     create_table :cafes do |t|
       t.string :name
       t.string :address
+      t.string :description
       t.string :brew_methods
       t.string :price_range
       t.string :bean_source
-
+      t.string :image_url
       t.timestamps
     end
   end
