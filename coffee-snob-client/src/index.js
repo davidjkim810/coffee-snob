92e80b1cae982b484aa365bce56573c6a77578e5
David Kim <davidjkim810@gmail.com>
2018-10-29 16:34:09 -0400

    created root reducer

--- coffee-snob-client/src/index.js
+++ coffee-snob-client/src/index.js
@@ -6,8 +6,13 @@ import App from './App';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
 
-
+const middleware = [thunk];
+const store = createStore(
+  rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
+)
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ coffee-snob-client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  cafes: cafesReducer,
+  comments: commentsReducer
+})
