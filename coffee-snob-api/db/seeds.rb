8ea142d417ba197d63ea218633ce9bb06f7f01df
David Kim <davidjkim810@gmail.com>
2018-10-29 15:14:14 -0400

    added seed data for cafes

--- coffee-snob-api/db/seeds.rb
+++ coffee-snob-api/db/seeds.rb
@@ -14,4 +14,20 @@
 # t.string :bean_source
 # t.string :image_url
 
-Cafe.create(name: "Cafe Trois", address: "211 Livingston St, Northvale, NJ 07647", description: "French-inspired cafe that offer french pastries, omelets, and sandwiches", brew_methods: "Espresso, Filter", bean_source: "Unknown", price_range: "Moderate", image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/cAk_FFM6_nm22pYLtI9c8A/o.jpg")
+Cafe.create(name: "Cafe Trois", address: "211 Livingston St, Northvale, NJ 07647", description: "French-inspired cafe that offer french pastries, omelets, and sandwiches", brew_methods: "Espresso, Drip", bean_source: "Unknown", price_range: "Moderate", image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/cAk_FFM6_nm22pYLtI9c8A/o.jpg")
+
+Cafe.create(name: "Patisserie Florentine", address: "234 Closter Dock Road, Closter, NJ 07624", description: "Patisserie Florentine brings a little of France, Stateside; the French bistro setting is a charming fusion of rustic & modern offering a convivial friendly atmosphere", brew_methods: "Espresso, Drip", bean_source: "Fresh Southern Jersey Roastery", price_range: "Moderate", image_url: "https://lh5.googleusercontent.com/p/AF1QipOludAnY7tnpKUgYR_OXo-aB7QBiFkhuojLe2RK=s408-k")
+
+Cafe.create(name: "Bunbury's Coffee Shop", address: "460 Piermont Avenue, Piermont, NY 10968", description: "Cozy coffeehouse featuring gourmet coffees, teas, breakfast pastries, sweets & sandwiches.", brew_methods: "Espresso, Drip", bean_source: "Pirsos coffee & Tea", price_range: "Moderate", image_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/13001173_10153419365356286_3129069132073888279_n.jpg?_nc_cat=100&_nc_ht=scontent-lga3-1.xx&oh=d75cb81c023e0b344171056f7ec37044&oe=5C801D81")
+
+Cafe.create(name: "Starbucks Reserve", address: "29 Vervalen Street, Closter, NJ 07624", description: "Seattle-based coffeehouse chain known for its signature roasts, light bites and WiFi availability.", brew_methods: "Espresso, Pour over, Drip", bean_source: "Starbucks Reserve Roasteries", price_range: "Moderate", image_url: "http://www.starbuckseverywhere.net/bigimages/34/DSCN34185.jpg")
+
+Cafe.create(name: "La Tabatiere", address: "303 Herbert Ave, Closter, NJ 07624", description: "La Tabatiere was founded almost 2 years ago by Chef Won, former Executive Chef of the renowned 2 Michelin starred Jungsik. His travels to France inspired him to start a neighborhood cafe where people would be able to savor time with family and friends over exquisite coffee and French pastries and breads.", brew_methods: "Espresso, Pour over, Drip", bean_source: "Stumptown", price_range: "Moderate", image_url: "http://www.starbuckseverywhere.net/bigimages/34/DSCN34185.jpg")
+
+Cafe.create(name: "Cool Beans International Coffee & Teas", address: "304 Kinderkamack Rd, Oradell, NJ 07649
+", description: "Hip, homey coffeehouse with late hours & live music serving desserts & light fare, plus beer & wine.
+", brew_methods: "Espresso, Drip", bean_source: "Stumptown", price_range: "Moderate", image_url: "http://www.starbuckseverywhere.net/bigimages/34/DSCN34185.jpg")
+
+Cafe.create(name: "Cool Beans International Coffee & Teas", address: "304 Kinderkamack Rd, Oradell, NJ 07649
+", description: "Hip, homey coffeehouse with late hours & live music serving desserts & light fare, plus beer & wine.
+", brew_methods: "Espresso, Drip", bean_source: "Stumptown", price_range: "Moderate", image_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/13043813_10207651310694406_8783943613228608072_n.jpg?_nc_cat=109&_nc_ht=scontent-lga3-1.xx&oh=43e3942d22406dd2658d23cbe7a59c67&oe=5C8A5AA8")
