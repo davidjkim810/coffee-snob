ead61392bc8fd3530520ba7c97247c673690e640
David Kim <davidjkim810@gmail.com>
2018-10-29 14:05:40 -0400

    created all actions for cafe controller

--- coffee-snob-api/app/controllers/cafes_controller.rb
+++ coffee-snob-api/app/controllers/cafes_controller.rb
@@ -1,22 +1,40 @@
 class CafesController < ApplicationController
+  before_action :set_cafe, only: [:show, :update, :destroy]
 
   def index
+    @cafes = Cafe.all
+    render json: @cafes
   end
 
   def show
+    render json: @cafe
   end
+
   def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      render json: @cafe
+    end
   end
 
   def update
+    if @cafe.update(cafe_params)
+      render json: @cafe
+    end
   end
 
   def destroy
+    @cafe.destroy
   end
 
   private
 
   def cafe_params
+    params.require(:cafe).permit(:name, :address, :description, :brew_methods, :price_range, :bean_source, :image_url)
+  end
+
+  def set_cafe
+    @cafe = Cafe.find(params[:id])
   end
 
 end
